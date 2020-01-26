cae097522cb0439ec224efc0a8d8e32d8db00e12
eb88ibyw <soroosh.arasteh@fau.de>
2020-01-26 20:52:40 +0100

    update to torchtext version

--- data/data_handler.py
+++ data/data_handler.py
@@ -4,22 +4,32 @@
 
 import numpy as np
 import random
-import os
-import csv
 import torch
-from torch.utils.data import Dataset
+from torchtext import data
 from Train_Test import Mode
 from configs.serde import read_config
+import pdb
 
 
-class data_provider(Dataset):
-    def __init__(self, cfg_path, dataset_name, size, mode=Mode.TRAIN, seed=1):
+
+
+class data_provider_V2():
+    '''
+    Packed padded sequences
+    Pre-trained embedding: glove.6B.100d
+    tokenizer: spacy
+    '''
+    def __init__(self, cfg_path, batch_size, split_ratio=0.8, max_vocab_size=25000, mode=Mode.TRAIN, seed=1):
         '''
         Args:
             cfg_path (string):
                 Config file path of the experiment
-            size (int):
-                No of messages to be used from the dataset
+            max_vocab_size (int):
+                The number of unique words in our training set is usually over 100,000,
+                which means that our one-hot vectors will have over 100,000 dimensions! SLOW TRAINIG!
+                We only take the top max_vocab_size most common words.
+            split_ratio (float):
+                train-valid splitting
             mode (enumeration Mode):
                 Nature of operation to be done with the data.
                 Possible inputs are Mode.PREDICT, Mode.TRAIN, Mode.VALID
@@ -28,120 +38,57 @@ class data_provider(Dataset):
         params = read_config(cfg_path)
         self.cfg_path = cfg_path
         self.mode = mode
-        self.datset_name = dataset_name
-        self.dataset_path = os.path.join(params['input_data_path'], dataset_name)
-        self.testset_path = os.path.join(params['test_data_path'], dataset_name)
-        self.size = size
-        self.input_tensor, self.target_tensor, self.vocab_size = self._init_dataset(seed)
+        self.seed = seed
+        self.split_ratio = split_ratio
+        self.max_vocab_size = max_vocab_size
+        self.dataset_path = params['input_data_path']
+        self.batch_size = batch_size
 
 
-    def __len__(self):
-        '''Returns length of the dataset'''
-        return self.size
-
-    def __getitem__(self, idx):
+    def data_loader(self):
         '''
-        Using self.input_tensor list and self.target_tensor list the argument value idx,
-        return inputs and labels(if applicable based on Mode)
-        The inputs and labels are returned in torch tensor format.
+        :include_lengths: Packed padded sequences: will make our RNN only process the non-padded elements of our sequence,
+            and for any padded element the `output` will be a zero tensor.
+        :tokenize: the "tokenization" (the act of splitting the string into discrete "tokens") should be done using the spaCy tokenizer.
         '''
-        X = self.input_tensor[idx]
-        Y = self.target_tensor[idx]
+        '''Packed padded sequences'''
+        TEXT = data.Field(tokenize='spacy', include_lengths=True)  # For saving the length of sentences
+        LABEL = data.LabelField()
 
-        # Converting input and label to tensor
-        x_input = torch.from_numpy(X)
-        y_input = torch.from_numpy(Y)
+        fields = [('id', None), ('user_id', None),  ('label', LABEL), ('text', TEXT)]
+        train_data, test_data = data.TabularDataset.splits(
+            path=self.dataset_path,
+            train='train_and_dev_b_2014.txt',
+            test='test_gold_b_2014_2015.txt',
+            format='tsv',  # tab separated value
+            fields=fields,
+            skip_header=False)
 
-        if self.mode == Mode.PREDICT:
-            return x_input
-        else:
-            return x_input, y_input
+        # validation data
+        train_data, valid_data = train_data.split(random_state=random.seed(self.seed), split_ratio=self.split_ratio)
 
+        # create the vocabulary only on the training set!!!
+        # vectors: instead of having our word embeddings initialized randomly, they are initialized with these pre-trained vectors.
+        # initialize words in your vocabulary but not in your pre-trained embeddings to Gaussian
+        TEXT.build_vocab(train_data, max_size=self.max_vocab_size,
+                         vectors="glove.6B.100d", unk_init=torch.Tensor.normal_)
+        LABEL.build_vocab(train_data)
 
-    def _init_dataset(self, seed):
-        ''' Collects the input and label arrays and saves them to a list.
-        @inspired_from: Elvis Saravia
-        '''
-        # Dataset path
-        if self.mode==Mode.VALID:
-            dataset_path = self.testset_path
-        else:
-            dataset_path = self.dataset_path
+        vocab_size = len(TEXT.vocab)
 
-        # Opening the data file
-        # SIDs = [] # tweet id
-        # UIDs = [] # user id
-        labels = []
-        messages = []
-        with open(dataset_path, 'r') as f:
-            reader = csv.reader(f, delimiter='\t')
-            for SID, UID, label, message in reader:
-                # SIDs.append(SID)
-                # UIDs.append(UID)
-                labels.append(label)
-                messages.append(message)
+        # for packed padded sequences all of the tensors within a batch need to be sorted by their lengths
+        train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits((
+            train_data, valid_data, test_data), batch_size=self.batch_size,
+            sort_within_batch=True, sort_key=lambda x: len(x.text))
 
-        if len(messages) < self.size:
-            print('The size you requested is more than the total available messages.')
-            self.size = len(messages)
+        return train_iterator, valid_iterator, test_iterator, vocab_size
 
-        elif len(messages) > self.size:
-            print('The size you requested is less than the total available messages. '
-                  'The desired number of messages will randomly be picked.')
-            random.seed(seed)
-            random.shuffle(messages)
-            messages = messages[:self.size]
-
-        # Building Vocabulary
-        vocab = set()
-        word2idx = {}
-        idx2word = {}
-
-        # Updating with individual tokens
-        for s in messages:
-            vocab.update(s.split(' '))
-
-        # Sort the vocab
-        vocab = sorted(vocab)
-
-        # Word to index mapping
-        word2idx['<pad>'] = 0   # add a padding token with index 0
-        for index, word in enumerate(vocab):
-            word2idx[word] = index + 1  # +1 because of pad token
-
-        # Index to word mapping
-        for word, index in word2idx.items():
-            idx2word[index] = word
-
-        # Converting data into tensors
-        input_tensor = [[word2idx[s] for s in es.split(' ')] for es in messages]
-
-        # Padding data to be of equal sizes
-        max_len = max(len(t) for t in input_tensor)
-        def pad_sequences(x, max_len):
-            padded = np.zeros((max_len), dtype=np.int64)
-            if len(x) > max_len: padded[:] = x[:max_len]
-            else: padded[:len(x)] = x
-            return padded
-        input_tensor = [pad_sequences(x, max_len) for x in input_tensor]
-
-        # Converting targets to one-hot encoding vectors
-        classes = list(set(labels))
-        # Binarizer
-        emotion_dict = {'positive': [1, 0, 0], 'negative': [0, 1, 0], 'neutral': [0, 0, 1]}
-
-        target_tensor = np.zeros((len(labels), len(classes)))
-        for i, label in enumerate(labels):
-            target_tensor[i] = (emotion_dict[label])
-
-        return input_tensor, target_tensor, len(word2idx)
 
 
 
 if __name__=='__main__':
     CONFIG_PATH = '/home/soroosh/Documents/Repositories/twitter_sentiment/configs/config.json'
-    full_train_dataset = data_provider(cfg_path=CONFIG_PATH, dataset_name='2014_b_train.txt', size=1000)
-    VOCAB_SIZE = full_train_dataset.vocab_size
-    train_size = int(0.8 * len(full_train_dataset))
-    valid_size = len(full_train_dataset) - train_size
-    train_dataset, test_dataset = torch.utils.data.random_split(full_train_dataset, [train_size, valid_size])
+    data_handler = data_provider_V2(cfg_path=CONFIG_PATH, batch_size=1, split_ratio=0.8, max_vocab_size=25000)
+    train_iterator, valid_iterator, test_iterator, vocab_size = data_handler.data_loader()
+    # pdb.set_trace()
+    # a=2

5f50f23950c17924156d5b5a847dcec9c82d0791
eb88ibyw <soroosh.arasteh@fau.de>
2020-02-01 21:24:02 +0100

    cleaned the code

--- data/summarizer.py
+++ data/summarizer.py
@@ -1,66 +1,64 @@
 '''
-"data_post_reply_withlabel.py" contains now the label of each reply, in the column 0.
-and neutral labels and pick the label which has the maximum
-number of appearance and assign to the label of tweet. Then remove all the repetitive tweets.
-(here we don't care about the replies and you can delete them if you want.
-
+here we don't care about the replies and we delete them.
 *** Our intended data structure here: Unique tweets and only one label for each tweet.
 @author: Mehrpad Monajem <mehrpad.monajem@fau.de>
 '''
 
 import pandas as pd
+import os
 
-path = '/home/mehrpad/Desktop/dl_seminar/git_seminar/data'
-path_new = path + '/post_reply'
 
-data = pd.read_csv(path + '/data_post_reply_withlabel.csv')
 
-print('finish reading data')
-print(len(data))
+def summarizer(data_path, input_file_name, output_file_name):
 
-data_new = data.drop(['id', 'user', 'reply'], axis=1)
-data_final = pd.DataFrame(columns=['label', 'tweet'])
+    data = pd.read_csv(os.path.join(data_path, input_file_name))
 
-it = iter(range(0, len(data_new)))
+    print('finish reading data\n')
+    print('length of the data:', len(data))
 
-for tweet in it:
+    data_new = data.drop(['id', 'user', 'reply'], axis=1)
+    data_final = pd.DataFrame(columns=['label', 'tweet'])
 
-    id = tweet
-    label_pos = 0
-    label_neg = 0
-    label_nat = 0
-    label = data_new.iloc[tweet][0]
-    tweet_text = data_new.iloc[tweet][1]
+    it = iter(range(0, len(data_new)))
 
-    if label == 'positive':
-        label_pos += 1
-    elif label == 'negative':
-        label_neg += 1
-    elif label == 'neutral':
-        label_nat += 1
+    for tweet in it:
 
-    while True:
-        tweet = next(it)
-        if tweet_text == data_new.iloc[tweet][1]:
-            if data_new.iloc[tweet][0] == 'positive':
-                label_pos += 1
-            elif data_new.iloc[tweet][0] == 'negative':
-                label_neg += 1
-            elif data_new.iloc[tweet][0] == 'neutral':
-                label_nat += 1
+        id = tweet
+        label_pos = 0
+        label_neg = 0
+        label_nat = 0
+        label = data_new.iloc[tweet][0]
+        tweet_text = data_new.iloc[tweet][1]
 
-        else:
-            break
-        if tweet + 1 >= len(data_new):
-            break
+        if label == 'positive':
+            label_pos += 1
+        elif label == 'negative':
+            label_neg += 1
+        elif label == 'neutral':
+            label_nat += 1
 
-    var = {label_pos: "positive", label_neg: "negative", label_nat: "neutral"}
-    label_final = var.get(max(var))
+        while True:
+            tweet = next(it)
+            if tweet_text == data_new.iloc[tweet][1]:
+                if data_new.iloc[tweet][0] == 'positive':
+                    label_pos += 1
+                elif data_new.iloc[tweet][0] == 'negative':
+                    label_neg += 1
+                elif data_new.iloc[tweet][0] == 'neutral':
+                    label_nat += 1
 
-    df = pd.DataFrame([[label_final, tweet_text]], columns=['label', 'tweet'])
-    data_final = data_final.append(df)
+            else:
+                break
+            if tweet + 1 >= len(data_new):
+                break
 
-data_final.to_csv(path + "/data_post_with_max_replies_label.csv", index=False)
+        var = {label_pos: "positive", label_neg: "negative", label_nat: "neutral"}
+        label_final = var.get(max(var))
+
+        df = pd.DataFrame([[label_final, tweet_text]], columns=['label', 'tweet'])
+        data_final = data_final.append(df)
+
+    data_final.to_csv(os.path.join(data_path, output_file_name), index=False)
 
 
 

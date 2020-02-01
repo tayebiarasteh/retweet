9857d34a3f92cb650933e3325296d2712b1a1a93
eb88ibyw <soroosh.arasteh@fau.de>
2020-02-01 22:40:56 +0100

    main_train_postreply() added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 '''
 This is the main function running the Training, Validation, Testing process.
 Set the hyper-parameters and model parameters here. [data parameters from config file]
+
 @author: Soroosh Tayebi Arasteh <soroosh.arasteh@fau.de>
 '''
 
@@ -30,7 +31,7 @@ def main_train():
     '''Main function for training + validation.'''
 
     '''Hyper-parameters'''
-    NUM_EPOCH = 15
+    NUM_EPOCH = 13
     LOSS_FUNCTION = CrossEntropyLoss
     OPTIMIZER = optim.Adam
     BATCH_SIZE = 32
@@ -46,7 +47,7 @@ def main_train():
 
         '''Prepare data'''
         data_handler = data_provider_V2(cfg_path=cfg_path, batch_size=BATCH_SIZE,
-                                        split_ratio=0.8, max_vocab_size=MAX_VOCAB_SIZE)
+                                        split_ratio=0.8, max_vocab_size=MAX_VOCAB_SIZE, mode=Mode.TRAIN)
         train_iterator, valid_iterator, vocab_size, PAD_IDX, UNK_IDX, pretrained_embeddings = data_handler.data_loader()
         '''Initialize trainer'''
         trainer = Training(cfg_path)
@@ -128,9 +129,11 @@ def main_reply_predict():
     EXPERIMENT_NAME = 'Adam_lr0.0005_max_vocab_size25000'
     params = open_experiment(EXPERIMENT_NAME)
     cfg_path = params['cfg_path']
+
     # Prepare the network parameters (use the same "max_vocab_size" as in training)
     data_handler_test = data_provider_V2(cfg_path=cfg_path, max_vocab_size=25000, mode=Mode.PREDICTION)
     labels, vocab_idx, vocab_size, PAD_IDX, UNK_IDX, pretrained_embeddings = data_handler_test.data_loader()
+
     # Initialize prediction
     predictor = Prediction(cfg_path)
     predictor.setup_model(model=biLSTM, vocab_size=vocab_size,
@@ -166,6 +169,49 @@ def main_reply_predict():
 
 
 
+def main_train_postreply():
+    '''
+    Main function for training + validation of the second part of the project:
+    Sentiment analysis of the Post-Replies.
+    '''
+    '''Hyper-parameters'''
+    NUM_EPOCH = 15
+    LOSS_FUNCTION = CrossEntropyLoss
+    OPTIMIZER = optim.Adam
+    BATCH_SIZE = 16
+    #max_vocab_size: takes the 25000 most frequent words as the vocab
+    parameters = dict(lr = [5e-4], max_vocab_size = [25000])
+    param_values = [v for v in parameters.values()]
+
+    '''Hyper-parameter testing'''
+    for lr, MAX_VOCAB_SIZE in product(*param_values):
+        # put the new experiment name here.
+        params = create_experiment("POSTREPLY_Adam_lr" + str(lr) + "_max_vocab_size" + str(MAX_VOCAB_SIZE))
+        cfg_path = params["cfg_path"]
+
+        '''Prepare data'''
+        data_handler = data_provider_PostReply(cfg_path=cfg_path, batch_size=BATCH_SIZE,
+                                        split_ratio=0.8, max_vocab_size=MAX_VOCAB_SIZE, mode=Mode.TRAIN)
+        train_iterator, valid_iterator, vocab_size, PAD_IDX, UNK_IDX, pretrained_embeddings = data_handler.data_loader()
+        '''Initialize trainer'''
+        trainer = Training(cfg_path)
+
+        '''Model parameters'''
+        optimiser_params = {'lr': lr}
+        EMBEDDING_DIM = 100
+        HIDDEN_DIM = 256
+        OUTPUT_DIM = 3
+        MODEL = biLSTM(vocab_size=vocab_size, embeddings=pretrained_embeddings, embedding_dim=EMBEDDING_DIM,
+                       hidden_dim=HIDDEN_DIM, output_dim=OUTPUT_DIM, pad_idx=PAD_IDX, unk_idx=UNK_IDX)
+        trainer.setup_model(model=MODEL, optimiser=OPTIMIZER,
+                            optimiser_params=optimiser_params, loss_function=LOSS_FUNCTION)
+        '''Execute Training'''
+        trainer.execute_training(train_loader=train_iterator, valid_loader=valid_iterator, num_epochs=NUM_EPOCH)
+
+
+
+
+
 def prediction_time(start_time, end_time):
     elapsed_time = end_time - start_time
     elapsed_mins = int(elapsed_time / 60)
@@ -177,7 +223,7 @@ def experiment_deleter():
     parameters = dict(lr = [5e-4], max_vocab_size = [25000])
     param_values = [v for v in parameters.values()]
     for lr, MAX_VOCAB_SIZE in product(*param_values):
-        delete_experiment("Adam_lr" + str(lr) + "_max_vocab_size" + str(MAX_VOCAB_SIZE))
+        delete_experiment("POSTREPLY_Adam_lr" + str(lr) + "_max_vocab_size" + str(MAX_VOCAB_SIZE))
 
 
 
@@ -186,4 +232,5 @@ if __name__ == '__main__':
     # main_train()
     # main_test()
     # main_manual_predict()
-    main_reply_predict()
+    # main_reply_predict()
+    main_train_postreply()

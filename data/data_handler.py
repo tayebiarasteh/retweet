3e0200c1a83da5f68e8b07aa0370ef89d66929bf
eb88ibyw <soroosh.arasteh@fau.de>
2020-02-06 23:56:18 +0100

    Revert back to the previous checkpoint

--- data/data_handler.py
+++ data/data_handler.py
@@ -11,6 +11,7 @@ from torchtext import data
 from Train_Test_Valid import Mode
 from configs.serde import read_config
 import os
+import pandas as pd
 import pdb
 
 
@@ -197,6 +198,62 @@ class data_provider_PostReply():
             return labels, vocab_idx, vocab_size, PAD_IDX, UNK_IDX, pretrained_embeddings
         else:
             return train_iterator, valid_iterator, vocab_size, PAD_IDX, UNK_IDX, pretrained_embeddings
+
+
+def summarizer(data_path, input_file_name, output_file_name):
+    data = pd.read_csv(os.path.join(data_path, input_file_name))
+    data_new = data.drop(['id', 'user', 'reply'], axis=1)
+    data_final = pd.DataFrame(columns=['label', 'tweet'])
+    it = iter(range(0, len(data_new)))
+    for tweet in it:
+
+        id = tweet
+        label_pos = 0
+        label_neg = 0
+        label_nat = 0
+        label = data_new.iloc[tweet][0]
+        tweet_text = data_new.iloc[tweet][1]
+
+        if label == 'positive':
+            label_pos += 1
+        elif label == 'negative':
+            label_neg += 1
+        elif label == 'neutral':
+            label_nat += 1
+
+        while True:
+            tweet = next(it)
+            if tweet_text == data_new.iloc[tweet][1]:
+                if data_new.iloc[tweet][0] == 'positive':
+                    label_pos += 1
+                elif data_new.iloc[tweet][0] == 'negative':
+                    label_neg += 1
+                elif data_new.iloc[tweet][0] == 'neutral':
+                    label_nat += 1
+            else:
+                break
+            if tweet + 1 >= len(data_new):
+                break
+        var = {label_pos: "positive", label_neg: "negative", label_nat: "neutral"}
+        label_final = var.get(max(var))
+
+        df = pd.DataFrame([[label_final, tweet_text]], columns=['label', 'tweet'])
+        data_final = data_final.append(df)
+
+    data_final.to_csv(os.path.join(data_path, output_file_name), index=False)
+
+
+def post_reply_downloader(list_of_word, max_num_tweets, mode='download'):
+    path = '/home/soroosh/Documents/Repositories/twitter_sentiment/data/preprocessing_utils/get_old_tweets_3-0.0.10'
+    max_tweets = max_num_tweets
+    list_word = list_of_word
+    for word in list_word:
+        print('The word is:', word,'- Max number of tweets:', max_tweets)
+        os.system(' python %s/GetOldTweets3.py --querysearch "%s" --lang en --toptweets --maxtweets %s'
+                  ' --output=%s/%s.csv' %(path, word, max_tweets, path, word))
+    if mode == 'test':
+        data = pd.read_csv('%s/%s.csv' % (path, list_word[0]))
+        return data
 
 
 
--- main.py
+++ main.py
@@ -13,11 +13,9 @@ import spacy
 
 # User Defined Modules
 from configs.serde import *
-from Train_Test_Valid import Training, Prediction
+from Train_Test_Valid import Training, Prediction, Mode
 from data.data_handler import *
-from data.post_reply_downloader import *
 from models.biLSTM import *
-from utils.summarizer import *
 
 #System Modules
 from itertools import product
@@ -32,17 +30,17 @@ def main_train():
     '''Main function for training + validation.'''
 
     # if we are resuming training on a model
-    RESUME = False
+    RESUME = True
 
     # Hyper-parameters
-    NUM_EPOCH = 10
+    NUM_EPOCH = 100
     LOSS_FUNCTION = CrossEntropyLoss
     OPTIMIZER = optim.Adam
     BATCH_SIZE = 32
     #max_vocab_size: takes the 25000 most frequent words as the vocab
     MAX_VOCAB_SIZE = 25000
-    lr = 5e-4
-    EXPERIMENT_NAME = "new2Adam_lr" + str(lr) + "_max_vocab_size" + str(MAX_VOCAB_SIZE)
+    lr = 5e-5
+    EXPERIMENT_NAME = "Adam_lr" + str(lr) + "_max_vocab_size" + str(MAX_VOCAB_SIZE)
 
     if RESUME == True:
         params = open_experiment(EXPERIMENT_NAME)
@@ -59,7 +57,7 @@ def main_train():
     trainer = Training(cfg_path, num_epochs=NUM_EPOCH, RESUME=RESUME)
 
     # Model parameters
-    optimiser_params = {'lr': lr}
+    optimiser_params = {'lr': lr, 'weight_decay': 1e-5}
     EMBEDDING_DIM = 100
     HIDDEN_DIM = 256
     OUTPUT_DIM = 3
@@ -79,7 +77,7 @@ def main_train():
 def main_test():
     '''Main function for testing'''
     # Configs
-    EXPERIMENT_NAME = 'new2Adam_lr0.0005_max_vocab_size25000'
+    EXPERIMENT_NAME = 'Adam_lr5e-05_max_vocab_size25000'
     params = open_experiment(EXPERIMENT_NAME)
     cfg_path = params['cfg_path']
 
@@ -100,76 +98,64 @@ def main_test():
 
 
 
-def main_manual_predict(PHRASE=None):
-    '''Manually predicts the polarity of the given sentence.'''
-
+def main_manual_predict(PHRASE=None, prediction_mode='Manualpart1'):
+    '''
+    Manually predicts the polarity of the given sentence.
+    :mode:
+        'Manualpart1' predicts the sentiment of the tweet (part 1 pf the project)
+        'Manualpart2' predicts the semtiment of the potential reply of the tweet (part 2)
+        Note that for each part you should give the correct experiment name to load the correct model for it.
+    '''
     if PHRASE == None:
         # Enter your phrase below here:
-        PHRASE = "How you doin? :D"
+        PHRASE = "it is just fuckin awesome!"
 
     # Configs
     start_time = time.time()
-    EXPERIMENT_NAME = 'new2Adam_lr0.0005_max_vocab_size25000'
+    if prediction_mode == 'Manualpart1':
+        EXPERIMENT_NAME = 'Adam_lr5e-05_max_vocab_size25000'
+    elif prediction_mode == 'Manualpart2':
+        EXPERIMENT_NAME = 'POSTREPLY_Adam_lr0.0008_max_vocab_size25000'
+
     params = open_experiment(EXPERIMENT_NAME)
     cfg_path = params['cfg_path']
+
     # Prepare the network parameters
     # use the same "max_vocab_size" as in training
-    data_handler_test = data_provider_V2(cfg_path=cfg_path, max_vocab_size=25000, mode=Mode.PREDICTION)
+    if prediction_mode == 'Manualpart1':
+        data_handler_test = data_provider_V2(cfg_path=cfg_path,
+                                             max_vocab_size=25000, mode=Mode.PREDICTION)
+    elif prediction_mode == 'Manualpart2':
+        data_handler_test = data_provider_PostReply(cfg_path=cfg_path,
+                                                    split_ratio=0.8, max_vocab_size=25000, mode=Mode.PREDICTION)
+
     labels, vocab_idx, vocab_size, PAD_IDX, UNK_IDX, pretrained_embeddings = data_handler_test.data_loader()
+
     # Initialize prediction
     predictor = Prediction(cfg_path)
     predictor.setup_model(model=biLSTM, vocab_size=vocab_size,
                           embeddings=pretrained_embeddings, pad_idx=PAD_IDX, unk_idx=UNK_IDX)
     # Execute Prediction
-    predictor.manual_predict(labels=labels, vocab_idx=vocab_idx, phrase=PHRASE, mode=Mode.PREDICTION)
+    predictor.manual_predict(labels=labels, vocab_idx=vocab_idx,
+                             phrase=PHRASE, mode=Mode.PREDICTION, prediction_mode=prediction_mode)
     # Duration
     end_time = time.time()
     test_mins, test_secs = prediction_time(start_time, end_time)
     print(f'Prediction Time: {test_mins}m {test_secs}s')
-
-
-
-def main_tweet_reply_manual_predict(PHRASE=None):
-    '''Manually predicts the reply sentiment that would be taught to receive.'''
-
-    if PHRASE == None:
-        # Enter your phrase below here:
-        PHRASE = "How you doin? :D"
-
-    # Configs
-    start_time = time.time()
-    EXPERIMENT_NAME = 'new2Adam_lr0.0005_max_vocab_size25000'
-    params = open_experiment(EXPERIMENT_NAME)
-    cfg_path = params['cfg_path']
-    # Prepare the network parameters
-    # use the same "max_vocab_size" as in training
-    data_handler_test = data_provider_V2(cfg_path=cfg_path, max_vocab_size=25000, mode=Mode.PREDICTION)
-    labels, vocab_idx, vocab_size, PAD_IDX, UNK_IDX, pretrained_embeddings = data_handler_test.data_loader()
-    # Initialize prediction
-    predictor = Prediction(cfg_path)
-    predictor.setup_model(model=biLSTM, vocab_size=vocab_size,
-                          embeddings=pretrained_embeddings, pad_idx=PAD_IDX, unk_idx=UNK_IDX)
-
-    # TODO: load the second model here
-    # Execute Prediction
-    predictor.manual_predict(labels=labels, vocab_idx=vocab_idx, phrase=PHRASE, mode=Mode.PREDICTION)
-    # Duration
-    end_time = time.time()
-    test_mins, test_secs = prediction_time(start_time, end_time)
-    print(f'Prediction Time: {test_mins}m {test_secs}s')
-
 
 
 
 def main_reply_predict():
     '''
     Manually predicts the polarity of the given replies,
-    which will be regarded as the labels for the corresponding tweets.
-    Note: first you need to create a csv file which you want to save the labels in.
+    which will be regarded as the labels for the corresponding tweets
+    and creates a labeled dataset of only tweets and corresponding labels.
+    Note: first you need to create a csv file which you want to save
+    the labels in ("data_post_reply_withlabel.csv").
     '''
     # Configs
     start_time = time.time()
-    EXPERIMENT_NAME = 'new2Adam_lr0.0005_max_vocab_size25000'
+    EXPERIMENT_NAME = 'Adam_lr5e-05_max_vocab_size25000'
     params = open_experiment(EXPERIMENT_NAME)
     cfg_path = params['cfg_path']
 
@@ -221,13 +207,13 @@ def main_train_postreply():
     RESUME = False
 
     # Hyper-parameters
-    NUM_EPOCH = 10
+    NUM_EPOCH = 100
     LOSS_FUNCTION = CrossEntropyLoss
     OPTIMIZER = optim.Adam
-    BATCH_SIZE = 32
+    BATCH_SIZE = 16
     #max_vocab_size: takes the 25000 most frequent words as the vocab
     MAX_VOCAB_SIZE = 25000
-    lr = 5e-4
+    lr = 8e-4
     EXPERIMENT_NAME = "POSTREPLY_Adam_lr" + str(lr) + "_max_vocab_size" + str(MAX_VOCAB_SIZE)
 
     if RESUME == True:
@@ -281,6 +267,6 @@ if __name__ == '__main__':
     # experiment_deleter()
     # main_train()
     # main_test()
-    # main_manual_predict()
-    main_reply_predict()
+    # main_manual_predict(prediction_mode='Manualpart1')
+    # main_reply_predict()
     main_train_postreply()
